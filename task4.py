bfd29593b60b9ebe96548957bb919c51eb11743e
KedaloEM <kedaloem@yandex.ru>
2015-09-30 23:50:49 +0300

    Added h1 tag

---
+++ task4.py
@@ -0,0 +1,14 @@
+A=[1,2,3,5,3,7,9,13]
+B=A
+C=A
+for elem in A:
+    if A.count(elem)==1:
+        print(elem,end=' ')
+print()
+
+for i in range(0,len(B),2):
+    B[i],B[i+1]=B[i+1],B[i]
+print(' '.join(map(str,B)))
+
+print(str(C[len(C)-1])+' '+' '.join(map(str,C[0:len(C)-1])))
+
