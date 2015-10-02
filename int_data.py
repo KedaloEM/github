bf85b8a02ff5c3cc5769524d73f8bd4cd1f37362
KedaloEM <kedaloem@yandex.ru>
2015-10-02 17:27:28 +0300

    Added h1 tag

---
+++ int_data.py
@@ -0,0 +1,7 @@
+﻿from random import *
+output=open('int_data.txt','w')
+
+
+for i  in range(1000000):
+    print(randint(1,100), file=output,end=' ')
+output.close()
--- task6.py
+++ task6.py
@@ -1,28 +1,22 @@
-float1=open('float_data.txt','r')
-output=open('output.txt','w')
-c=(float1.read(6))
-s=0
+float1=open('float_data.txt', 'r')
+A=[]
+summ=0
 d=0
-i=0
-m=float(c)
-mal=float(c)
-while len(c)>0:
-    if float(c)>m:
-        m=float(c)
-        r=i
-    if float(c)<mal:
-        mal=float(c)
-        w=i
-    s+=float(c)
-    i+=1
-    d+=(float(c))**2
-    c=(float1.read(6))
-c=float1.read(6)
-q=s/1000000
-print(q)
-print(((d/1000000)-(q)**2)**0.5)
-float1.close()
-print(m,r)
-print(mal,w)
+B=float1.readlines()   
+s=len(B[0].split( ))
+for i in range(len(B)):
+    A=A+list(map(float, B[i].split( )))  
+for i in range(len(A)):
+    summ=summ+A[i]
+sred=summ/len(A)
+for i in range(len(A)):
+    d+=(A[i]**2)
+sredot=(d/len(A)-sred**2)**0.5
+print(sred)
+print(sredot)
+print(max(A),A.count(max(A)))
+print(min(A),A.count(min(A)))
+float1.close()    
 
-
+    
+    
