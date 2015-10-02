3fea24634aeec77c86633b97a19fee6e31e5a92f
KedaloEM <kedaloem@yandex.ru>
2015-10-02 16:25:54 +0300

    Delete task5.py

--- task5.py
+++
@@ -1,9 +0,0 @@
-float1=open('float_data.txt','w')
-from random import *
-for i in range(0,1000000,1):
-    float1.write(str(randint(0,9))+str(randint(0,9))+'.'+str(randint(0,9))+str(randint(0,9))+' ')
-float1.close()
-integer=open('int_data.txt','w')
-for i in range(0,1000000,1):
-    integer.write(str(randint(0,100))+' ')
-integer.close()
