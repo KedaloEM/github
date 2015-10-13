d78c5d752ce62ab15c841e5a10ecc4079d8c6231
KedaloEM <kedaloem@yandex.ru>
2015-10-13 11:19:50 +0300

    second

--- Practics7/task5.py
+++ Practics7/task5.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 x= np.arange(-20 , 25.01 , 0.01)
-c=input()
-b=eval(c)
-plt.plot(x , b)
 plt.xkcd()
+c=input()
+b=eval('c')
+plt.plot(x , b)
+plt.show()
 
