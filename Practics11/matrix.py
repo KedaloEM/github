96e0d6d302aa2a75a06dabc75f818ede9661e6be
KedaloEM <kedaloem@yandex.ru>
2015-11-24 21:09:45 +0300

    Update matrix.py

--- Practics11/matrix.py
+++ Practics11/matrix.py
@@ -1,22 +1,25 @@
 __author__ = 'student'
 class Matrix:
     def __init__(self, m, n=None):
-        if (not isinstance(m,int) and not isinstance(m,list)) or (not isinstance(n,int)):
+       if type(m)!=list:
+            if type(m)!=int:
+                raise ValueError()
+        if type(n)!=int:
+            if n!=None:
+                raise ValueError()
+        if type(n)==int and n<1:
             raise ValueError()
-        if m<1 or n<1:
+        if type(n)==int and len(n)<1:
             raise ValueError()
-        if n==None and isinstance(m,list):
+        if n==None:
+            matr=m
             self.matr=m
-            self.stroki=len(m)
-            self.stolbcy=len(m[1])
-        if m>0 and n>0 and type(m)==int and type(n)==int:
+            self.stroki=len(matr)
+            self.stolbcy=len(matr[0])
+        else:
             self.stroki=m
             self.stolbcy=n
-            self.matr=[[0]*int(self.stolbcy) for i in range(int(self.stroki))]
-        if n==None and type(m)==int and m>0:
-            self.stroki=m
-            self.stolbcy=m
-            self.matr=[[0]*(int(self.stroki)) for i in range(int(self.stroki))]
+            self.matr=[[0]*n for i in range(m)]
 
 
     def get(self, i,j):
@@ -26,14 +29,14 @@ class Matrix:
             return(self.matr[i-1][j-1])
 
     def __add__(self, other):
-        if (self.stroki==other.stroki) and (self.stolbcy==other.stolbcy):
+        if (self.stroki!=other.stroki) or (self.stolbcy!=other.stolbcy):
+            raise ValueError()
+        else:
             Summ=[[0]*(int(self.stroki)) for i in range(int(self.stroki))]
             for i in range(int(self.stroki)):
                 for j in range(int(self.stolbcy)):
                     Summ[i][j]=self.matr[i][j]+other.matr[i][j]
-        else:
-            raise Exception()
-        return(Summ)
+        return(Matrix(Summ))
     def __eq__(self, other):
         ravn=1
         if self.m==other.m and (self.n==other.n):
