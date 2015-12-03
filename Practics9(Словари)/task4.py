5f6da08e1fbc07c7c1aae351c34953b1e41f9447
KedaloEM <kedaloem@yandex.ru>
2015-12-04 01:15:13 +0300

    second commit

--- Practics9(Словари)/task4.py
+++ Practics9(Словари)/task4.py
@@ -1,35 +1,24 @@
-Words={}
-enru=open('en-ru.txt','r')
+text=open('en-ru.txt','r')
+ourWords={}
+for line in text:
+    line=line.replace(' ','').replace(',',' ').replace('   ','').replace('.','').replace('\t','').replace('\n','').split('-')
+    ourWords[line[0]]=line[1]
+text.close()
+
 input=open('input.txt','r')
 output=open('output.txt','w')
-S=enru.readlines()
-P=()
-for elem in S:
-    P=elem.split('-')
-    Words[P[0]]=P[1]
-B=input.read()
-C=B.split()
-print(C)
-for i in range(len(C)):
-    if C[i] in Words:
-        q=Words[C[i]]
-        print(C[i],q)
-        print(q,file=output,end=' ')
-    elif C[i].lower() in Words:
-        if C[i]==C[i].title():
-            q=Words[C[i].lower()].title()
-            print(q,file=output,end=' ')
-    elif C[i][0:(len(C[i])-1):1] in Words:
-        q=Words[C[i][0:(len(C[i])-1):1]]+'.'
-        print(q,file=output,end=' ') 
-        print('',file=output)
-    elif C[i][0:(len(C[i])-1):1].lower() in Words:
-        if C[i][0:(len(C[i])-1):1]==C[i][0:(len(C[i])-1):1].title():
-            q=Words[C[i][0:(len(C[i])-1):1].lower()].title()+'.'
-            print(q,file=output,end=' ') 
-            print('',file=output)
-    else:
-        print(C[i],file=output)
-input.close()
-output.close()
+trans=[]
+for line in  input.read().split():
+     line=line.replace('.','').replace(',',' ')
+     line=line.lower()
+     if line.lower() in ourWords:
+         trans.append(ourWords[line])
+     else:
+         trans.append(line)
 
+for elem in trans:
+    print(elem, end=' ')
+    output.write(elem)
+    output.write(' ')
+output.close()
+input.close()
