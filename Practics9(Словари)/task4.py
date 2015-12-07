17c199ad360bdb3c25e11699a2fca66d8863efe4
KedaloEM <kedaloem@yandex.ru>
2015-12-07 18:25:27 +0300

    second commit

--- Practics9(Словари)/task4.py
+++ Practics9(Словари)/task4.py
@@ -1,20 +1,22 @@
-text=open('en-ru.txt','r')
-ourWords={}
+text = open('en-ru.txt', 'r')
+ourWords = {}
 for line in text:
-    line=line.replace(' ','').replace(',',' ').replace('   ','').replace('.','').replace('\t','').replace('\n','').split('-')
-    ourWords[line[0]]=line[1]
+    line = line.replace(' ', '').replace(',', ' ').replace('   ', '').replace('.', '').replace('\t', '').replace('\n',
+                                                                                                                 '').split(
+        '-')
+    ourWords[line[0]] = line[1]
 text.close()
 
-input=open('input.txt','r')
-output=open('output.txt','w')
-trans=[]
-for line in  input.read().split():
-     line=line.replace('.','').replace(',',' ')
-     line=line.lower()
-     if line.lower() in ourWords:
-         trans.append(ourWords[line])
-     else:
-         trans.append(line)
+input = open('input.txt', 'r')
+output = open('output.txt', 'w')
+trans = []
+for line in input.read().split():
+    line = line.replace('.', '').replace(',', ' ')
+    line = line.lower()
+    if line.lower() in ourWords:
+        trans.append(ourWords[line])
+    else:
+        trans.append(line)
 
 for elem in trans:
     print(elem, end=' ')
